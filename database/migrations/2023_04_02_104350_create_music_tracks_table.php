b5e1f9645191923294144091c7bedf63c6b05cf7
UmbertoDV <biccoumby2002@hotmail.com>
2023-04-02 12:53:57 +0200

    create music tracks

---
+++ database/migrations/2023_04_02_104350_create_music_tracks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('music_tracks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('album')->nullable();
+            $table->string('author');
+            $table->string('editor');
+            $table->float('length', 2, 2);
+            $table->text('poster')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('music_tracks');
+    }
+};
